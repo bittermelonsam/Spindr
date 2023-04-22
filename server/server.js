eff352fbb70b1ef6ad8922f534c2e7d4fcf9101e
sam <sam@cps-macbook-pro.lan>
2023-04-22 15:08:26 -0700

    Oauth token generation successful
    Co-authored-by: Jessica jessicavo@github.com
    Co-authored-by: Sam bittermelonsam@github.com

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const querystring = require('node:querystring'); 
+const querystring = require('node:querystring');
 const crypto = require('crypto');
 const axios = require('axios'); //easier library for fetching
 const PORT = process.env.PORT || 3000;
-
+const fetch = require('node-fetch');
 
 //this allows you to access .env files to read data
 //client ID is stored in .env file for security
@@ -21,38 +21,47 @@ const clientSecret = 'YOUR_CLIENT_SECRET'; */
 
 const redirectUri = process.env.REDIRECT_URI;
 
-//These define the parameters that the user will provide us access to. 
+//These define the parameters that the user will provide us access to.
 //Additional scopes can be found here: https://developer.spotify.com/documentation/web-api/concepts/scopes
-const scopes = ['playlist-read-private', 'playlist-modify-public', 'playlist-modify-private', 'user-read-playback-state', 'user-modify-playback-state'];
+const scopes = [
+  'playlist-read-private',
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 //generate random string to use in state property as recommended by Spotify authorization guidelines
 const generateRandomString = (length) => {
-  return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0,length);
-}
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
 
 const stateKey = 'spotify_auth_state';
 
 //GET request to spotify's authorization page
 app.get('/login', (req, res) => {
-//Below is for PKCE, to implement later
+  //Below is for PKCE, to implement later
   // const codeVerifier = generateRandomString();
   // const codeChallenge = base64UrlEncode(crypto.createHash('sha256').update(codeVerifier).digest());
   const state = generateRandomString(16);
 
-  const authorizationUrl = 'https://accounts.spotify.com/authorize?' +
+  const authorizationUrl =
+    'https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code', //required
       client_id: clientId, //required
       scope: scopes.join(' '), //optional scopes
       redirect_uri: redirectUri, //uri that we set when we requested client id on spotify's create app
-    //also for PKCE:
+      //also for PKCE:
       // code_challenge: codeChallenge,
       // code_challenge_method: 'S256',
-      state: state
+      state: state,
     });
   res.cookie(stateKey, state);
   res.redirect(authorizationUrl);
@@ -64,40 +73,64 @@ app.get('/callback', (req, res) => {
   const state = req.query.state || null;
   const storedState = req.cookies ? req.cookies[stateKey] : null;
 
+  // app.get('/callback', async (req, res) => {
+  //   fetch('https://accounts.spotify.com/api/token', {
+  //     method: 'POST',
+  //     headers: {
+  //       Authorization: `Basic ${new Buffer.from(
+  //         `${clientId}:${clientSecret}}`
+  //       ).toString('base64')}`,
+  //       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+  //     },
+  //     body: new URLSearchParams({
+  //       grant_type: 'client_credentials',
+  //       code: req.query.code,
+  //       redirect_uri: redirectUri,
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (data) {
+  //         res.cookie('accessToken', data.access_token);
+  //         // res.status(200).redirect('http://localhost:3000/');
+  //       }
+  //     });
+  // });
+
   axios({
     method: 'post',
     url: 'https://accounts.spotify.com/api/token',
-    data: {
+    data: querystring.stringify({
       grant_type: 'authorization_code',
       code: code,
-      redirect_uri: redirectUri
-    },
+      redirect_uri: redirectUri,
+    }),
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
-      //a buffer is a temporary storage area in memory that is used to hold data while it is being transferred from one place to another. 
+      //a buffer is a temporary storage area in memory that is used to hold data while it is being transferred from one place to another.
       //a buffer object is a specific implementation of a buffer that is commonly used in programming to represent and manipulate binary data.
-      // 'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-      Authorization: `Basic ${new Buffer.from(`${clientId}:${clientSecret}}`).toString('base64')}`,
+      Authorization:
+        'Basic ' +
+        new Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
     },
+
+    // json: true,
   })
-  .then(response => {
-    console.log('STATUS CODE: ', response.staus)
-    if (response.status === 200) {
-      //response using <pre> will display data received from spotify without linebreaks, whitespace
-      //axios stores data returned by requests in the data property of the response obj
-      res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-    } else {
-      //if not 200 response, server will give back what spotify is serving
-      res.send(response);
-    }
-  })
-  .catch(error => {
-    res.send(error);
-  });
+    .then((response) => {
+      console.log('STATUS CODE: ', response.staus);
+      if (response.status === 200) {
+        //response using <pre> will display data received from spotify without linebreaks, whitespace
+        //axios stores data returned by requests in the data property of the response obj
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        //if not 200 response, server will give back what spotify is serving
+        res.send(response);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
-
-
-
 
 // app.get('/callback', async (req, res) => {
 //   const code = req.query.code || null;
@@ -138,7 +171,7 @@ app.get('/callback', (req, res) => {
 //CONNECT TO THE DATABASE
 
 app.get('/test', (req, res) => {
-  console.log('bobo');
+  // console.log('bobo');
   res.status(200).json('is this wog?');
 });
 
@@ -147,7 +180,7 @@ app.post('/test', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('this will be populated by the goated frontend team (Wed and Tay)');
+  res.send('this will be populated by the goated frontend team (Wes and Tay)');
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
