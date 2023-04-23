8737c3c6d5ceef3aa91a4a1c8914b5f2c784f72d
sam <sam@cps-MacBook-Pro.attlocal.net>
2023-04-23 11:03:49 -0700

    storing of auth token in cookies and creating a playlist when token is valid

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
+// const path = require('path');
+// const mongoose = require('mongoose');
 const querystring = require('node:querystring');
 const crypto = require('crypto');
 const axios = require('axios'); //easier library for fetching
 const PORT = process.env.PORT || 3000;
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+//had trouble finding the docs from spotify but found this and seems to work: https://jmperezperez.com/spotify-web-api-js/#src-spotify-web-api.js-constr.prototype.setaccesstoken
+const spotifyApi = require('spotify-web-api-node'); // apparently this will allow us to make a spotify object with a method that sets the access token
+const https = require('https');
+// const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+app.use(cookieParser());
+
+// Load SSL certificate
+// const options = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem'),
+// };
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 //this allows you to access .env files to read data
 //client ID is stored in .env file for security
 require('dotenv').config();
@@ -71,6 +88,8 @@ app.get('/login', (req, res) => {
 app.get('/callback', (req, res) => {
   const code = req.query.code || null; //pulling out the authorization code after oauthing
   const state = req.query.state || null;
+
+  //note to jessica: we added this earlier. too tired to understand if it actually plays a more important part now that we are using cookies
   const storedState = req.cookies ? req.cookies[stateKey] : null; //if we have cookies, then pull out the value from stateKey property
 
   //can use Node fetch but axios seems to be a bit more simple and more commonly used with Spotify api
@@ -89,7 +108,7 @@ app.get('/callback', (req, res) => {
       //spent a couple hours with this stupid syntax so dont mess this up!!!!
       //Update clientID and clientSecret with your updated ones and check to see if token is being generated
 
-      /* Token should look like this:
+      /* Token obj should look like this:
 {
   "access_token": "ACCESS TOKEN", <-- your access token
   "token_type": "Bearer",
@@ -106,10 +125,42 @@ app.get('/callback', (req, res) => {
     .then((response) => {
       console.log('TOKEN: ', response.data);
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
-        //response using <pre> will display data received from spotify without linebreaks, whitespace
-        //axios stores data returned by requests in the data property of the response obj
-        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        //we destructure the response.data which contains our token data
+        const { access_token, token_type, refresh_token } = response.data;
+
+        //note to jessica: im able to get the access token cookie to generate but not the other 2 for some reason
+        //nvm, access token is a response cookie, apparently type and refresh tokens are on the req cookie
+        res.cookie('access_token', access_token, {
+          httpOnly: true,
+          secure: true, //only transmit cookies over HTTPS
+          maxAge: 3600000, //cookie will expire in an hour
+        });
+
+        //this doesn't feel very DRY..
+        res.cookie('token_type', token_type, {
+          httpOnly: true,
+          secure: true, //only transmit cookies over HTTPS
+          maxAge: 3600000, //cookie will expire in an hour
+        });
+
+        res.cookie('refresh_token', refresh_token, {
+          httpOnly: true,
+          secure: true, //only transmit cookies over HTTPS
+          maxAge: 3600000, //cookie will expire in an hour
+        });
+
+        axios
+          .get('https://api.spotify.com/v1/me', {
+            headers: {
+              Authorization: `${token_type} ${access_token}`,
+            },
+          })
+
+          .then((response) => {
+            //response using <pre> will display data received from spotify without linebreaks, whitespace
+            //axios stores data returned by requests in the data property of the response obj
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          });
       } else {
         //if not 200 response, server will give back what spotify is serving
         res.send(response);
@@ -120,8 +171,72 @@ app.get('/callback', (req, res) => {
     });
 });
 
+app.get('/createPlaylist', (req, res) => {
+  //if access token exists in req cookie then assign it, otherwise set it to null
+  const access_token = req.cookies ? req.cookies['access_token'] : null;
+
+  //check to see if token exists
+  if (!access_token) {
+    return res.send('NO TOKENS HERE, TRY AGAIN LOSER.');
+  }
+
+  const spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(access_token);
+
+  spotifyApi
+    .createPlaylist('THIS IS A PLAYLIST GENERATED BY MY APPLICATION HOW COOL', {
+      public: false,
+    })
+    .then((data) => {
+      console.log('created a playlist boiiiiii');
+      console.log(data.body);
+      res.send('PLAYLIST CREATE COMPLETE. WOO.');
+    })
+    .catch((error) => {
+      console.error("didn't work :( :", error);
+      res.send('ERROR, NO PLAYLIST');
+    });
+});
+
+/* SHAPE OF DATA RESPONSE SAMPLE DISPLAYING OTHER ACCESSIBLE KEYS
+{
+  collaborative: false,
+  description: null,
+  external_urls: {
+    spotify: 'https://open.spotify.com/playlist/09LxYI70frBYbnmUiZoULq'
+  },
+  followers: { href: null, total: 0 },
+  href: 'https://api.spotify.com/v1/playlists/09LxYI70frBYbnmUiZoULq',
+  id: '09LxYI70frBYbnmUiZoULq',
+  images: [],
+  name: 'THIS IS A PLAYLIST GENERATED BY MY APPLICATION HOW COOL',
+  owner: {
+    display_name: 'Sam G. Phan',
+    external_urls: { spotify: 'https://open.spotify.com/user/samcurz' },
+    href: 'https://api.spotify.com/v1/users/samcurz',
+    id: 'samcurz',
+    type: 'user',
+    uri: 'spotify:user:samcurz'
+  },
+  primary_color: null,
+  public: false,
+  snapshot_id: 'MSwyNDBkNzE3MTc5NDkzYTA0ZTUyZjcyN2Y0NjRjYzQ5OTZmZjljZTU1',
+  tracks: {
+    href: 'https://api.spotify.com/v1/playlists/09LxYI70frBYbnmUiZoULq/tracks',
+    items: [],
+    limit: 100,
+    next: null,
+    offset: 0,
+    previous: null,
+    total: 0
+  },
+  type: 'playlist',
+  uri: 'spotify:playlist:09LxYI70frBYbnmUiZoULq'
+}
+*/
+
 //TODO:
-//CONNECT TO THE DATABASE
+//CONNECT TO THE DATABASE TO STORE ACCESS TOKEN INFO ON USER AS WELL AS OTHER DATA LIKE PREVIOUS SONG SELECTIONS AND RECS
 
 app.get('/test', (req, res) => {
   // console.log('bobo');
